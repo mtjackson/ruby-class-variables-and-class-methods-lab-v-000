885d1c2dcb35fd7d61b788ff5d62865a2b0c8bfb
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-30 01:16:15 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,6 @@ class Song
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre] == nil
-        genre_count[genre] = ""
         genre_count[genre] = 1
       else
         genre_count[genre] += 1
@@ -54,7 +53,6 @@ class Song
     artist_count = {}
     @@artists.each do |artist|
       if artist_count[artist] == nil
-        artist_count[artist] = ""
         artist_count[artist] = 1
       else
         artist_count[artist] += 1
