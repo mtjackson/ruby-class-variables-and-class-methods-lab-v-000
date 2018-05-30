5945df1aba3e8a5a3bd3e90cf0065268cfd3a898
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-30 01:06:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,7 +8,8 @@ class Song
     @artist = artist
     @genre = genre
     @@count += 1
-    @@artists << artist
+    if @@artists.include?(artist) == false
+      @@artists << artist
     end
     @@genres << genre
 
