b29052f4078b6918c4bf210d44a2ad79bbdef897
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-30 01:15:31 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -51,6 +51,10 @@ class Song
   end
 
   def self.artist_count
-    artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist] == nil
+        artist_count[artist] = ""
+        artist_count[artist] = 1
 
 end
