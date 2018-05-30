4e72d4e3cfad65aaf6f15809e55dd91ecdae061d
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-30 00:59:56 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,19 @@
 class Song
-  attr_accessor :name, :artist, :genre
   def initialize(name, artist, genre)
-  #  @name = name
-  #  @artist = artist
-  #  @genre = genre
+    @name = name
+    @artist = artist
+    @genre = genre
   end
 
   def name
     @name
   end
 
-  #def artist
-  #  @artist
-  #end
+  def artist
+    @artist
+  end
 
-  #def genre
-  #  @genre
-  #end
+  def genre
+    @genre
+  end
 end
